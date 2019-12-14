547afc294b88eb3b91134219380b14899c1c4a33
ciemekk92 <przemyslaw.reducha@gmail.com>
2019-12-14 10:30:12 +0100

    MaterialUI

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,8 +1,48 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import 'typeface-roboto';
+import { Typography, TextField, Button } from '@material-ui/core/';
 
-function App() {
-    return <div className="App"></div>;
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="App">
+        <Typography variant="h3" align="center" gutterBottom>
+          Przykładowy H3 - typeface Roboto
+        </Typography>
+        <TextField
+          fullWidth
+          gutterBottom
+          style={{ margin: '20px' }}
+        ></TextField>
+        <Button variant="contained" style={{ margin: '20px' }}>
+          Default
+        </Button>
+        <Button variant="contained" color="primary" style={{ margin: '20px' }}>
+          Primary
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ margin: '20px' }}
+        >
+          Secondary
+        </Button>
+        <Button variant="contained" disabled style={{ margin: '20px' }}>
+          Disabled
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          href="#contained-buttons"
+          style={{ margin: '20px' }}
+        >
+          Link
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-web/src/index.js
+++ react-web/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { indigo, blue } from '@material-ui/core/colors';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: blue
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
