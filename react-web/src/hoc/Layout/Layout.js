3c6c3cb876f708a9022953c4de7c8478d26cada0
ciemekk92 <przemyslaw.reducha@gmail.com>
2019-12-15 11:12:56 +0100

    Tabela, dane do zmiany i wystylizowania

--- react-web/src/hoc/Layout/Layout.js
+++ react-web/src/hoc/Layout/Layout.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import 'typeface-roboto';
 import Aux from './../Auxiliary/Auxiliary';
 import MiniDrawer from '../../components/MiniDrawer/MiniDrawer';
+import Table from '../../components/Table/Table';
 
 class Layout extends Component {
   render() {
     return (
       <Aux>
         <MiniDrawer />
+        <Table />
       </Aux>
     );
   }
